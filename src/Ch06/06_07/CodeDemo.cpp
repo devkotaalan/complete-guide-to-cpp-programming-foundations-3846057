f955ffc7f454d4d72caedc85eddd536e9ad3e391
Alan Devkota <152534162+devkotaalan@users.noreply.github.com>
2026-02-13 10:00:33 +0000

    31st commit

--- src/Ch06/06_07/CodeDemo.cpp
+++ src/Ch06/06_07/CodeDemo.cpp
@@ -8,21 +8,44 @@
 
 // Function to calculate score based on milliseconds difference 
 // You still need to make the bonus multiplier optional with a default value
-double CalculateScore(int millisecondsDiff, double bonusMultiplier){
+double CalculateScore(int millisecondsDiff, double bonusMultiplier = 1.0){
     double score = 0.0;
 
     // Write your code here
-    
+    if (millisecondsDiff <= 50.0){
+        score = 100.0 * bonusMultiplier;
+    }
+    else if (millisecondsDiff <= 100.0){
+        score = 70.0 * bonusMultiplier;
+    }
+    else if (millisecondsDiff <= 200.0){
+        score = 50.0* bonusMultiplier;
+    }
+    else {
+        score = 0;
+    }
     return score;
 }
 
 // Overloaded function to calculate score based on seconds difference 
 // You still need to make the bonus multiplier optional with a default value
-double CalculateScore(double secondsDiff, double bonusMultiplier){
+double CalculateScore(double secondsDiff, double bonusMultiplier = 1.5){
     double score = 0.0;
     
     // Write your code here
-
+    double millisecondsDiff = secondsDiff * 1000;
+    if (millisecondsDiff <= 50.0){
+        score = 100.0 * bonusMultiplier;
+    }
+    else if (millisecondsDiff <= 100.0){
+        score = 70.0 * bonusMultiplier;
+    }
+    else if (millisecondsDiff <= 200.0){
+        score = 50.0* bonusMultiplier;
+    }
+    else {
+        score = 0;
+    }
     return score;
 }
 

94e35d7586afec430363b249856e3440391469fd
Alan Devkota <152534162+devkotaalan@users.noreply.github.com>
2026-02-22 14:09:53 +0000

    36th commit

--- src/Ch07/07_10/CodeDemo.cpp
+++ src/Ch07/07_10/CodeDemo.cpp
@@ -15,16 +15,26 @@ class Person{
 
     void Eat(float calories){
         this->energy = this->energy + (calories * 7.0/200.0);
+        if (energy > 100) 
+            energy = 100;
     }
 
     void Play(float minutes){
         this->happiness = this->happiness + minutes/2.0;
         this->energy = this->energy - minutes/3.0;
+        if (happiness > 100) 
+            happiness = 100;
+        if (energy < 0) 
+            energy = 0;
     }
 
     void Sleep(float hours){
         this->energy = this->energy + hours*3.75;
         this->health = this->health + hours*2.5;
+        if (energy > 100) 
+            energy = 100;
+        if (health > 100) 
+            health = 100;
     }
 
     float GetEnergy() const {
@@ -38,7 +48,7 @@ class Person{
     float GetHealth() const {
         return this->health;
     }
-    
+
     private:
     std::string name;
     float energy;
@@ -48,14 +58,24 @@ class Person{
 
 int main(){
     // Example 1
-    std::string name = "Alice";
-    float energy = 40;
-    float happiness = 22;
-    float health = 80;
+    // std::string name = "Alice";
+    // float energy = 40;
+    // float happiness = 22;
+    // float health = 80;
 
-    float calories = 300;
-    float playMinutes = 120;
-    float sleepHours = 5;
+    // float calories = 300;
+    // float playMinutes = 120;
+    // float sleepHours = 5;
+
+    // Example 2
+    std::string name = "Joe";
+    float energy = 50;
+    float happiness = 70;
+    float health = 60;
+
+    float calories = 1000;
+    float playMinutes = 60;
+    float sleepHours = 8;
 
     Person sporty(name, energy, happiness, health);
     sporty.Eat(calories);
